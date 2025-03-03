742ab0ced7e12d9af5e9890800e875af263441ee
juan-zarandon <zarandonheraldo@gmail.com>
2025-03-03 19:05:28 -0300

    función para sortear los amigos
    se realiza la ultima parte del sorteo y mostrar resultado en la pagina html q ve el usuario

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-//Crear un array para almacenar los nombres
+// Crear un array para almacenar los nombres
 let amigos = [];
 
-//Implementa una función para agregar amigos
+// Implementa una función para agregar amigos
 function agregarAmigo() {
   const inputAmigo = document.getElementById("amigo");
   const nombreAmigo = inputAmigo.value.trim();
@@ -10,14 +10,14 @@ function agregarAmigo() {
     alert("Debes ingresar un nombre");
     return;
   }
-  //validacion de nombre que no sea duplicado
+  // validacion de nombre que no sea duplicado
   if (amigos.includes(nombreAmigo)) {
     alert(`El nombre ${nombreAmigo} ya está en la lista`);
     return;
   }
-  //agregando nombre al array
+  // agregando nombre al array
   amigos.push(nombreAmigo);
-  //limpiando campo de entrada
+  // limpiando campo de entrada
   inputAmigo.value = "";
 
   // actualizando la lista de amigos
@@ -33,9 +33,28 @@ function actualizarlista() {
 
   // Recorrer el array con un ciclo for
   for (let i = 0; i < amigos.length; i++) {
+    // Corregido: let i
     const li = document.createElement("li");
     li.textContent = amigos[i];
 
     listaAmigos.appendChild(li);
   }
+}
+
+// seguimos el desafio en la tercera parte finalizando con la funcion para sortear el amigo secreto
+
+// funcion para sortear amigo secreto
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("Debes agregar al menos un amigo para sortear el amigo secreto"); // validacion de que haya al menos un amigo
+    return;
+  }
+
+  // generacion de indice aleatorio
+  const indiceAleatorio = Math.floor(Math.random() * amigos.length); // generamos un numero aleatorio entre 0 y la cantidad de amigos en el array
+  const amigoSecreto = amigos[indiceAleatorio]; // obtiene el amigo secreto a partir del indice aleatorio generado anteriormente
+
+  // Mostrar el resultado en el HTML
+  const resultado = document.getElementById("resultado");
+  resultado.textContent = `Amigo secreto: ${amigoSecreto}`;
 }
